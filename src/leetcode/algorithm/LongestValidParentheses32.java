7884fa79d0f67e28e53961ea5bc7ed90bf04d453
JJC <1275596938@qq.com>
2022-07-31 21:10:20 +0800

    棧stack

--- src/leetcode/algorithm/LongestValidParentheses32.java
+++ src/leetcode/algorithm/LongestValidParentheses32.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 
 public class LongestValidParentheses32 {
     public static void main(String[] args) {
-        System.out.println(MySolution.longestValidParentheses(")()())))))))))(((((()))))))"));
+        System.out.println(MySolution.longestValidParentheses(")()()))))))))(((((()))))))"));
     }
     static class MySolution {
         public static int longestValidParentheses(String s) {
