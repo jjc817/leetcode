ea182e9fd24b38a034343fd264d10c64a47bc120
JJC <1275596938@qq.com>
2022-08-02 20:55:55 +0800

    DFS

---
+++ src/leetcode/algorithm/Exist79.java
@@ -0,0 +1,13 @@
+//package leetcode.algorithm;
+//
+//public class Exist79 {
+//    public static void main(String[] args) {
+//        MySolution.exist(new char[][]{{'A','B','C','D'},{'S','F','C','S'},{'A','D','E','F'}},"ABCCED");
+//    }
+//    static class MySolution {
+//        public static boolean exist(char[][] board, String word) {
+//            dfs();
+//        }
+//
+//    }
+//}
--- src/leetcode/algorithm/Subsets78.java
+++ src/leetcode/algorithm/Subsets78.java
@@ -5,43 +5,24 @@ import java.util.List;
 
 public class Subsets78 {
     public static void main(String[] args) {
-        System.out.println(MySolution.subsets(new int[]{1, 2, 3,}));
+         System.out.println(MySolution.subsets(new int[]{1, 2, 3,}));
     }
     static class MySolution {
         static List<List<Integer>> res = new ArrayList<>();
         public static List<List<Integer>> subsets(int[] nums) {
-            backtrack(new ArrayList<>(),nums, nums.length,0);
+            dfs(new ArrayList<>(),nums,0);
             return res;
         }
-        public static void backtrack(List<Integer> path,int[] nums,int len,int i) {
+        public static void dfs(List<Integer> path,int[] nums,int i) {
             if (i == nums.length) {
                 res.add(new ArrayList<>(path));
                 return;
             }
             path.add(nums[i]);
-            backtrack(path, nums, len, i + 1);
+            dfs(path, nums, i + 1);
             path.remove(path.size() - 1);
-            backtrack(path, nums, len, i + 1);
+            dfs(path, nums, i + 1);
         }
     }
-   static class Solution {
-        static List<Integer> t = new ArrayList<Integer>();
-        static List<List<Integer>> ans = new ArrayList<List<Integer>>();
 
-        public static List<List<Integer>> subsets(int[] nums) {
-            dfs(0, nums);
-            return ans;
-        }
-
-        static public void dfs(int cur, int[] nums) {
-            if (cur == nums.length) {
-                ans.add(new ArrayList<Integer>(t));
-                return;
-            }
-            t.add(nums[cur]);
-            dfs(cur + 1, nums);
-            t.remove(t.size() - 1);
-            dfs(cur + 1, nums);
-        }
-    }
 }
