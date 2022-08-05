21a2bdd5b96966a7c359f239d46e860612faf7f5
JJC <1275596938@qq.com>
2022-08-06 00:53:58 +0800

    ******

---
+++ src/leetcode/algorithm/_139wordBreak.java
@@ -0,0 +1,28 @@
+package leetcode.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _139wordBreak {
+    public static void main(String[] args) {
+        List<String> word = new ArrayList<>();
+        word.add("let");
+        word.add("code");
+        System.out.println(MySolution.wordBreak("leetcode", word));
+    }
+    static class MySolution {
+        public static boolean wordBreak(String s, List<String> wordDict) {
+            boolean[] dp = new boolean[s.length() + 1];
+            dp[0] =true;
+            for (int i = 1; i <= s.length(); i++) {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j] && wordDict.contains(s.substring(j,i))){
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[dp.length - 1];
+        }
+    }
+}
