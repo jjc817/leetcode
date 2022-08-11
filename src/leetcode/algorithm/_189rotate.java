81bb665c17e1de424a487056e95debdf6aa273e6
JJC <1275596938@qq.com>
2022-08-11 20:18:39 +0800

    ******

---
+++ src/leetcode/algorithm/_189rotate.java
@@ -0,0 +1,14 @@
+package leetcode.algorithm;
+
+public class _189rotate {
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            newArr[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(newArr, 0, nums, 0, n);
+    }
+
+}
